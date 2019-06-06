2561b4c6269692926627cbfa03c0aba094c46af2
belaifa_el_hussein <belaifahocine@gmail.com>
2019-06-06 20:49:02 +0200

    fix bug in scrollspy

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $('.navbar-nav li a').click(function (event) {
+//  event.preventDefault();
+//  console.log('scroll');
+//  $($(this).attr('href'))[0].scrollIntoView();
+//  scrollBy(0, -30);
+// });
+
+// $('body').scrollspy({
+//  target: '#navbarResponsive',
+//  offset: 50
+// })
